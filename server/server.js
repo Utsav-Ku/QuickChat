4b851f32a4994623452948e42c7559faa7ff7867
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-06-29 17:02:16 +0530

    Add user authentication routes for signup, login, and profile updates

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
+import userRouter from "./routes/userRoutes.js";
 
 //create Express app and Http server
 const app = express();
@@ -12,7 +13,9 @@ const server = http.createServer(app);
 app.use(express.json({limit: "4mb"}));
 app.use(cors());
 
+//Routes Setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
+app.use("/api/auth", userRouter);
 
 //connect to mongoDb
 await connectDB();
