1038d7aef2bc1b13159b7db262c104c9fade4728
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-06-29 17:37:49 +0530

    Add message routes for user message retrieval and seen status updates

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import http from "http";
 import { connectDB } from "./lib/db.js";
 import userRouter from "./routes/userRoutes.js";
+import messageRouter from "./routes/messageRoutes.js";
 
 //create Express app and Http server
 const app = express();
@@ -16,6 +17,7 @@ app.use(cors());
 //Routes Setup
 app.use("/api/status", (req, res) => res.send("Server is live"));
 app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 //connect to mongoDb
 await connectDB();
