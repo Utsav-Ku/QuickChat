3dd8baeaa0c3ac94670ccdb9d5e413d2702cd8ca
Utsav-Ku <152358955+Utsav-Ku@users.noreply.github.com>
2025-06-30 13:06:23 +0530

    Add cloudinary integration for image uploads in sendMessage function

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -1,5 +1,6 @@
 import Message from "../models/Message.js";
 import User from "../models/User.js";
+import cloudinary from "../lib/cloudinary.js";
 
 
 //Get all users except the logged in user
@@ -59,6 +60,34 @@ export const markMessageAsSeen = async (req, res) => {
         res.json({ success: true })
 
     } catch(error){
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+}
+
+//Send message to selected user
+export const sendMessage = async (req, res) => {
+    try {
+        const { text, image } = req.body;
+        const receiverId = req.params._id;
+        const senderId = req.user._id;
+
+        let imageUrl;
+        if(image){
+            const uploadResponse = cloudinary.uploader.upload(image);
+            imageUrl = uploadResponse.secure_url
+        }
+
+        const newMessage = await Message.create({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        res.json({ success: true, newMessage });
+
+    } catch (error) {
         console.log(error.message);
         res.json({ success: false, message: error.message });
     }
